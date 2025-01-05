b24f5c94ffa9d02a64e49938e7f986d4b3225fae
Emerson De Farias Santos <emerson.farias@unimedjp.com.br>
2025-01-05 14:38:22 -0300

    inclusion of card component

---
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { faDroplet, faTemperatureDown, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: []
+})
+export class WeatherCardComponent {
+  @Input() weatherDataInput!: WeatherDatas;
+
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+  
+}
