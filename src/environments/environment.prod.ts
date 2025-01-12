90e6a7526209fc0e305d656af58b4b5026d13786
Emerson De Farias Santos <emerson.farias@unimedjp.com.br>
2025-01-11 23:19:05 -0300

    separating prod and dev environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://api.openweathermap.org/data/2.5/weather', // URL da API para produção
+    featureFlag: true,                 // Pode desativar o recurso em produção
+    ApiKey: '6382bcc26479515eedc5eecb7474992f'   // Chave da API
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://api.openweathermap.org/data/2.5/weather',  // URL da API local para desenvolvimento
+    featureFlag: true,                // Exemplo de flag de recurso
+    ApiKey: '6382bcc26479515eedc5eecb7474992f'  // Chave da API 
+  };
